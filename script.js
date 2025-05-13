6f10eb607297e91586454c7577d085bfa646a07c
nunoabraao <nunoabraao@hotmail.com>
2025-05-13 13:36:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function girarRoleta() {
+  const resultadoEl = document.getElementById("resultado");
+
+  const numero = Math.random() * 100;
+
+  let resultado = "";
+
+  if (numero < 45) {
+    resultado = "🔴 Terrorista (T)";
+  } else if (numero < 90) {
+    resultado = "🔵 Counter-Terrorista (CT)";
+  } else {
+    resultado = "🟡 Dado (0)";
+  }
+
+  resultadoEl.textContent = "Resultado: " + resultado;
+}
